334a9bb1db08828ef4139c9ca4359a9a8739e8b5
Yann Gregoire <yann.gregoire@yahoo.fr>
2018-11-09 17:03:17 +0100

    MC models, controllers, views OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "starting seeding..."
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+100.times do
+  Cocktail.create(
+    name: Faker::Superhero.name)
+end
+
+100.times do
+  Ingredient.create(
+    name: Faker::Name.first_name)
+end
+
+100.times do
+  Dose.create(
+    description: "#{(1..20).to_a.sample} cl",
+    ingredient_id: (1..30).to_a.sample,
+    cocktail_id: (1..30).to_a.sample)
+end
+
+puts "seeding done!"
